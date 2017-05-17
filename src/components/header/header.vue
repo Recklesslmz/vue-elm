8c23954284d2ae5fff67eb299d02fbcf30f6d865
Recklesslmz <936838918@qq.com>
2017-05-17 22:36:31 +0800

    add

---
+++ src/components/header/header.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="content">
+      <div class="avatar">
+
+      </div>
+
+    </div>
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  export default {
+    data(){
+      return {}
+    },
+    methods:{
+
+    }
+  }
+
+</script>
+<style lang="scss">
+  .content{
+    height:103px;
+    background:rgba(7,17,27,.5);
+  }
+
+</style>
---
+++ src/pages/food.vue
@@ -0,0 +1,88 @@
+<template>
+  <div>
+    <div>
+      <v-header></v-header>
+    </div>
+    <div class="tab">
+      <div :class="[tabList,{tabListChecked:chooseIndex==index}]" @click="choose(index)"
+           v-for='(item,index) in tab'>
+        {{item.name}}
+      </div>
+    </div>
+    <div>
+      <router-view></router-view>
+    </div>
+
+  </div>
+
+</template>
+
+<script type="text/ecmascript-6">
+  import header from '../components/header/header'
+  export default {
+    data(){
+      return {
+        tab: [
+          {name: '商品', type: 1},
+          {name: '评价', type: 1},
+          {name: '商家', type: 1}
+        ],
+        tabList: 'tabList',
+        chooseIndex: ''
+      }
+    },
+    beforeCreate(){
+      document.getElementsByTagName('body')[0].style.background = '#faf5f7'
+    },
+    created(){
+      this.getInfo()
+    },
+    methods: {
+      choose(index){
+        this.chooseIndex = index
+        switch (index) {
+          case 0:
+            this.$router.push({path: '/'})
+            break
+          case 1:
+            this.$router.push({path: '/ratings'})
+            break
+          case 2:
+            this.$router.push({path: '/seller'})
+            break
+        }
+      },
+      getInfo(){
+        this.$http.get('/api/goods').then((response) => {
+          console.log(response)
+        });
+      }
+    },
+    components: {
+      'v-header': header
+    }
+  }
+
+</script>
+<style lang="scss">
+  .tab {
+    display: flex;
+    text-align: center;
+    width: 100%;
+    height: 40px;
+    background: #fff;
+    .tabList {
+      flex: 1;
+      line-height: 40px;
+      font: {
+        size: 14px;
+      }
+    ;
+      color: rgb(77, 85, 93);
+    }
+    .tabListChecked {
+      color: rgb(240, 20, 20);
+    }
+  }
+
+</style>
