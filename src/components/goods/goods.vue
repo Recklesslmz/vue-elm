e125a470304404d722afbd771d8d8c83e4630e75
Recklesslmz <936838918@qq.com>
2017-05-21 16:58:56 +0800

    点餐界面

--- src/components/goods/goods.vue
+++ src/components/goods/goods.vue
@@ -1,20 +1,54 @@
 <template>
   <div class="goods">
-    <div class="menu-wrapper">
-        <div v-for="item in foods">
-          <div class="text">{{item.name}}</div>
-        </div>
+    <div class="menu-wrapper" ref="menuWrapper">
+      <ul>
+        <li class="menu-item" :class="['menu-item',{'current':currentIndex === index}]" v-for="(item,index) in foods">
+          <span class="text">{{item.name}}</span>
+        </li>
+      </ul>
     </div>
-    <div class="foods-wrapper"></div>
+    <div class="foods-wrapper" ref="foodsWrapper">
+      <ul>
+        <li v-for="item in foods" class="food-list" ref="foodList">
+          <h1 class="title">
+            {{item.name}}
+          </h1>
+          <ul>
+            <li v-for="food in item.foods" class="food-item border-common">
+              <div class="icon">
+                <img width="57" height="57" :src="food.icon"/>
+              </div>
+              <div class="content">
+                <h2 class="name">{{food.name}}</h2>
+                <p class="desc">{{food.description}}</p>
+                <div class="extra">
+                  <span class="count">月售{{food.sellCount}}份</span>
+                  <span>好评率{{food.rating}}%</span>
+                </div>
+                <div class="price">
+                  <span class="now">￥{{food.price}}</span>
+                  <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+                </div>
+
+              </div>
+            </li>
+          </ul>
+        </li>
+      </ul>
+
+    </div>
   </div>
 
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
   export default{
     data(){
       return {
-          foods:[]
+        foods: [],
+        listHeight: [],
+        scrollY: 0
       }
     },
     created(){
@@ -22,38 +56,201 @@
     },
     methods: {
       getGoods(){
-        let _this = this
         this.$http.get('/api/goods').then((response) => {
           if (response.data.errno === 0) {
-              _this.foods = response.data.data
+            this.foods = response.data.data
+            this.$nextTick(() => {
+              this._initScroll()
+              this._calculateHeight()
+            });
             console.log(response)
           }
         });
+      },
+      _initScroll(){
+        this.menuScroll = new BScroll(this.$refs.menuWrapper, {
+          click: true
+        })
+        this.foodsScroll = new BScroll(this.$refs.foodsWrapper, {
+          click: true,
+          probeType: 3
+        })
+        this.foodsScroll.on('scroll', (pos) => {
+          this.scrollY = Math.abs(Math.round(pos.y))
+        })
+      },
+      _calculateHeight(){
+        let foodList = this.$refs.foodList
+        let height = 0
+        this.listHeight.push(height)
+        for (let i = 0; i < foodList.length; i++) {
+          let item = foodList[i]
+          height += item.clientHeight
+          this.listHeight.push(height);
+        }
+        console.log(this.listHeight[11])
       }
+    },
+    computed: {
+      currentIndex(){
+        for (let i = 0; i < this.listHeight.length; i++) {
+          let height1 = this.listHeight[i]
+          let height2 = this.listHeight[i + 1]
+          if (!height2 || (this.scrollY >= height1 && this.scrollY < height2)) {
+            return i
+          }
+
+        }
+        return 0
+      }
+
     }
   }
 
 </script>
 <style lang="scss">
-  .goods{
+  .goods {
     display: flex;
     position: absolute;
     width: 100%;
     overflow: hidden;
-    top:173px;
-    bottom:46px;
-    .menu-wrapper{
+    top: 173px;
+    bottom: 46px;
+    .menu-wrapper {
       flex: 0 0 80px;
-      background:#f3f5f7;
-      .text{
-        line-height:30px;
+      background: #f3f5f7;
+      .menu-item {
+        height: 54px;
+        width: 56px;
+        display: table;
+        margin: {
+          left: 12px;
+        }
+      ;
+        border-bottom: 1px solid rgba(7, 17, 27, .2);
+        .text {
+          display: table-cell;
+          line-height: 15px;
+          font: {
+            size: 12px;
+          }
+        ;
+          vertical-align: middle;
+        }
+        &.current{
+         background: white;
+          position: relative;
+          z-index: 10;
+          .text{
+            border:none;
+          }
+        }
+
       }
 
     }
-    .foods-wrapper{
+    .foods-wrapper {
+      flex: 1;
+      background: #fff;
+      .title {
+        padding: {
+          left: 14px;
+        }
+      ;
+        height: 26px;
+        line-height: 26px;
+        border: {
+          left: 2px solid #d9dde1;
+        }
+      ;
+        font: {
+          size: 12px;
+        }
+      ;
+        color: rgb(147, 153, 159);
+        background: #f3f5f7;
+      }
+      .food-item {
+        display: flex;
+        margin: 18px;
+        padding-bottom: 18px;
+        .icon {
+          flex: 0 0 57px;
+          margin-right: 10px;
+        }
+        .content {
+          flex: 1;
+          .name {
+            margin: 2px 0 8px 0;
+            font: {
+              size: 14px;
+            }
+          ;
+            color: rgb(7, 17, 27);
+            line-height: 14px;
+          }
+          .desc {
+            font: {
+              size: 10px;
+            }
+            line-height: 10px;
+            color: rgb(147, 153, 159);
+          }
+          .extra {
+            margin: {
+              top: 5px;
+            }
+          ;
+            .count {
+              margin: {
+                right: 12px;
+              }
+            ;
+            }
+            span {
+              font: {
+                size: 10px;
+              }
+              line-height: 10px;
+              color: rgb(147, 153, 159);
+            }
+          }
+          .price {
+            line-height: 24px;
+            .now {
+              font: {
+                weight: 700;
+              }
+            ;
+              margin: {
+                right: 18px;
+              }
+            ;
+              font: {
+                size: 14px;
+              }
+            ;
+              color: #ff0000;
+            }
+            .old {
+              font: {
+                weight: 700;
+              }
+            ;
+              text-decoration: line-through;
+              font: {
+                size: 10px;
+              }
+            ;
+            }
+          }
+        }
+      }
+      .border-common {
+        border-bottom: 1px solid rgba(7, 17, 27, .1);
+      }
 
     }
-
 
   }
 
